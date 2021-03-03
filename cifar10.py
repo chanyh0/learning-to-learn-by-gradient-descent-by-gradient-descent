9436ae8740667bce1596779c250016395764c345
Ch4nYH <hfercxx@hotmail.com>
2021-03-04 00:39:43 +0800

    update

--- cifar10.py
+++ cifar10.py
@@ -190,26 +190,29 @@ def fit_optimizer(target_cls, target_to_opt, preproc=False, unroll=20, optim_it=
     best_loss = 100000000000000000
     
     for _ in tqdm(range(n_epochs), 'epochs'):
-        #for _ in tqdm(range(20), 'iterations'):
-        #    do_fit(opt_net, meta_opt, target_cls, target_to_opt, unroll, optim_it, n_epochs, out_mul, should_train=True)
-        
+        print(train)
+        for _ in tqdm(range(1)):
+            do_fit(opt_net, meta_opt, target_cls, target_to_opt, unroll, optim_it, n_epochs, out_mul, should_train=True)
+        '''
         if test_target is not None:
             loss = (np.mean([
                 np.sum(do_fit(opt_net, meta_opt, target_cls, test_target, unroll, optim_it, n_epochs, out_mul, should_train=False))
-                for _ in tqdm(range(n_tests), 'tests')
+                for _ in tqdm(range(1))
             ]))
         else:
             loss = (np.mean([
                 np.sum(do_fit(opt_net, meta_opt, target_cls, target_to_opt, unroll, optim_it, n_epochs, out_mul, should_train=False))
-                for _ in tqdm(range(n_tests), 'tests')
+                for _ in tqdm(range(1))
             ]))
         print(loss)
         if loss < best_loss:
             print(best_loss, loss)
             best_loss = loss
             best_net = copy.deepcopy(opt_net.state_dict())
-            
+        
     return best_loss, best_net
+    '''
+    return 0, 0
   
 
 class CIFAR10Loss:

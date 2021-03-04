b333a89b88b1f7163ec63a656e6956561a1db968
Ch4nYH <hfercxx@hotmail.com>
2021-03-04 09:46:40 +0800

    p

--- resnet_meta.py
+++ resnet_meta.py
@@ -197,11 +197,11 @@ class ResNet(MetaModule):
     def _forward_impl(self, x):
         # See note [TorchScript super()]
         x = self.conv1(x)
-        #x = self.bn1(x)
-        #x = self.relu(x)
-        #x = self.maxpool(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
 
-        #x = self.layer1(x)
+        x = self.layer1(x)
         #x = self.layer2(x)
         #x = self.layer3(x)
         #x = self.layer4(x)

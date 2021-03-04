3aaad2921f8c265bcf40553d4c35087e398b4302
Ch4nYH <hfercxx@hotmail.com>
2021-03-04 10:14:27 +0800

    p

--- resnet_meta.py
+++ resnet_meta.py
@@ -143,9 +143,9 @@ class ResNet(MetaModule):
         self.bn1 = norm_layer(self.inplanes)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        #elf.layer1 = self._make_layer(block, 64, layers[0])
-        #self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
-        #                               dilate=replace_stride_with_dilation[0])
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
+                                       dilate=replace_stride_with_dilation[0])
         #self.layer3 = self._make_layer(block, 256, layers[2], stride=2,
         #                               dilate=replace_stride_with_dilation[1])
         #self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
@@ -202,8 +202,8 @@ class ResNet(MetaModule):
         x = self.relu(x)
         x = self.maxpool(x)
 
-        #x = self.layer1(x)
-        #x = self.layer2(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
         #x = self.layer3(x)
         #x = self.layer4(x)
 
